dc640eb607afb3e348092234aa2d947960804563
MinmusLin <minmuslin@outlook.com>
2024-06-09 15:37:08 +0800

    Update shapes

--- storylingokids/lib/app/lists/shapes_list.dart
+++ storylingokids/lib/app/lists/shapes_list.dart
@@ -3,7 +3,7 @@
  * File Name:     shapes_list.dart
  * File Function: 形状列表
  * Author:        林继申
- * Update Date:   2024-06-08
+ * Update Date:   2024-06-09
  * License:       MIT License
  */
 
@@ -17,11 +17,6 @@ List<ShapesModel> shapesList = [
     audio: 'assets/audios/shapes/Circle.mp3',
   ),
   ShapesModel(
-    name: 'Square',
-    image: 'assets/images/shapes/Square.png',
-    audio: 'assets/audios/shapes/Square.mp3',
-  ),
-  ShapesModel(
     name: 'Rectangle',
     image: 'assets/images/shapes/Rectangle.png',
     audio: 'assets/audios/shapes/Rectangle.mp3',
@@ -32,14 +27,9 @@ List<ShapesModel> shapesList = [
     audio: 'assets/audios/shapes/Triangle.mp3',
   ),
   ShapesModel(
-    name: 'Oval',
-    image: 'assets/images/shapes/Oval.png',
-    audio: 'assets/audios/shapes/Oval.mp3',
-  ),
-  ShapesModel(
-    name: 'Diamond',
-    image: 'assets/images/shapes/Diamond.png',
-    audio: 'assets/audios/shapes/Diamond.mp3',
+    name: 'Pentagon',
+    image: 'assets/images/shapes/Pentagon.png',
+    audio: 'assets/audios/shapes/Pentagon.mp3',
   ),
   ShapesModel(
     name: 'Hexagon',
@@ -47,9 +37,14 @@ List<ShapesModel> shapesList = [
     audio: 'assets/audios/shapes/Hexagon.mp3',
   ),
   ShapesModel(
-    name: 'Pentagon',
-    image: 'assets/images/shapes/Pentagon.png',
-    audio: 'assets/audios/shapes/Pentagon.mp3',
+    name: 'Octagon',
+    image: 'assets/images/shapes/Octagon.png',
+    audio: 'assets/audios/shapes/Octagon.mp3',
+  ),
+  ShapesModel(
+    name: 'Oval',
+    image: 'assets/images/shapes/Oval.png',
+    audio: 'assets/audios/shapes/Oval.mp3',
   ),
   ShapesModel(
     name: 'Star',
@@ -57,8 +52,13 @@ List<ShapesModel> shapesList = [
     audio: 'assets/audios/shapes/Star.mp3',
   ),
   ShapesModel(
-    name: 'Trapezoid',
-    image: 'assets/images/shapes/Trapezoid.png',
-    audio: 'assets/audios/shapes/Trapezoid.mp3',
+    name: 'Diamond',
+    image: 'assets/images/shapes/Diamond.png',
+    audio: 'assets/audios/shapes/Diamond.mp3',
+  ),
+  ShapesModel(
+    name: 'Cube',
+    image: 'assets/images/shapes/Cube.png',
+    audio: 'assets/audios/shapes/Cube.mp3',
   ),
 ];
